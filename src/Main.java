1e1e816af676f1ad8fc7e2970c644805af23e47b
Sebastian Garces Palacio <segarces23@gmail.com>
2018-02-20 11:02:51 -0500

    Initial Data Structures

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+class Queue {
+    LinkedList<String> queue = new LinkedList<>();
+
+    public void insert (String data) {
+        queue.add(data);
+    }
+
+    public String retrieve () {
+        String value = queue.getFirst();
+        queue.removeFirst();
+        return value;
+    }
+}
+
+public class Main {
+    public static void main (String [] args) {
+        String [] data = {"A", "B", "C", "D", "E"};
+        Queue queue = new Queue();
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < data.length; i++) {
+            queue.insert(data[i]);
+            stack.push(data[i]);
+        }
+
+        System.out.println("Retrieving Queue:");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(queue.retrieve());
+        }
+
+        System.out.println("<---------->");
+        System.out.println("Retrieving Stack:");
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
